cc66bb4bc828540ec2ae02ea47247eaf2068658a
Hypnos-E <76909450+Hypnos-E@users.noreply.github.com>
2021-10-20 23:09:14 +0300

    Update hmm.lua

--- hmm.lua
+++ hmm.lua
@@ -21,6 +21,7 @@ IPlist =
 	{"winterv1halxx45", "213.142.156.68"}, 
 	{"luciferv2x44221", "213.226.119.91"}, 
 	{"luciferv2x44221", "213.226.119.54"}, 
+	{"chivaskeyv2244", "78.111.111.137"}, 
 	{"WinterKey4466Qqxr", "45.131.1.61"} 
     }
 setElementData(root, "protect", 1)
